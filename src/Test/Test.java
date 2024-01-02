4d0f5202689e09a9e5322fbebe07d94abad9778b
Shelley_Koifman <46498251+skr2001@users.noreply.github.com>
2024-01-02 21:12:06 +0200

    Update 1

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,5 +1,24 @@
 package Test;
 
 public class Test {
+	public static void main(String[] args) {
+		/*
+		multi-line comment
+		*/
+		int i; //*
+		int j = 1;
+		boolean bool = false;
+		double d = 1.56;
+		char c = 'a';
+		String s = "a";
+		i = 5; //**
+		System.out.println("(1): " + i + " " + s);
+		System.out.println("(2): " + bool);
+		System.out.println("(3): " + d);
+		System.out.println("(4): " + c);
+		System.out.println("(5): " + c + j);
+		System.out.println("(6): " + (c + j));
+		System.out.println("(7): " + (char)(c + j));
+	}
 
 }
